fbfcc418c0f2b29d5703db4fe2bcfde7f1c45539
Микола Мосенцев <Mykola.Mosensev@cit.khpi.edu.ua>
2024-04-28 09:41:43 +0300

    Finish Task number 2

--- Mosentsev_IKM_621b_3_Lab_01/Form1.cs
+++ Mosentsev_IKM_621b_3_Lab_01/Form1.cs
@@ -13,6 +13,7 @@ namespace Mosentsev_IKM_621b_3_Lab_01
     public partial class Form1 : Form
     {
         private bool Mode; // Режим дозволу / заборони введення даних
+        private MajorWork MajorObject; // Створення об'єкта класу MajorWork
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +28,10 @@ namespace Mosentsev_IKM_621b_3_Lab_01
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            About A = new About(); // створення форми About
+            A.tAbout.Start();
+            A.ShowDialog(); // відображення діалогового вікна About
+            MajorObject = new MajorWork();
             this.Mode = true;
         }
 
@@ -46,6 +51,9 @@ namespace Mosentsev_IKM_621b_3_Lab_01
                 tClock.Stop();
                 bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
                 this.Mode = true;
+                MajorObject.Write(tbInput.Text);// Запис даних у об'єкт
+                MajorObject.Task();// Обробка даних
+                label1.Text = MajorObject.Read();// Відображення результату
             }
         }
 
