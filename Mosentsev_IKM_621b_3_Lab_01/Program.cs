eec86b74d81730e23f7b949e55ec3d9bb8422c8a
Микола Мосенцев <Mykola.Mosensev@cit.khpi.edu.ua>
2024-04-28 09:11:49 +0300

    Добавьте файлы проекта.

---
+++ Mosentsev_IKM_621b_3_Lab_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mosentsev_IKM_621b_3_Lab_01
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
